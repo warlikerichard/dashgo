bf3af14a892c934c5c42a2a4dc8cd204abb536cd
Warlike Richard <warlikerichard@hotmail.com>
2022-06-05 13:56:39 -0300

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,43 @@
+import { Flex, Button, Stack, FormControl } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Hello world</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+
+      <Flex as="form"
+        w="100%" maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <Input name="email" label="E-mail" type="email"/>
+          </FormControl>
+
+          <FormControl>
+              <Input name="password" label="Senha" type="password"/>
+          </FormControl>
+        </Stack>
+
+        <Button
+          type='submit'
+          mt={6}
+          colorScheme='pink'
+          size='lg'
+          >
+
+          Entrar
+        </Button>
+      </Flex>
+
+    </Flex>
   )
 }
