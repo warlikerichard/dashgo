484f7e4e0a0159cef557458ee8f4046e7cff7d7d
Warlike Richard <warlikerichard@hotmail.com>
2022-06-23 02:56:35 -0300

    react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,20 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from '../../components/Header'
 import Pagination from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar'
+import { useEffect } from 'react';
+import { useQuery } from 'react-query'
 
 export default function UserList(){
+    const {data, isLoading, error} = useQuery('users', async () => {
+        const response = await fetch('/api/users')
+        const data = await response.json()
+
+        return data;
+    })
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
@@ -29,7 +38,7 @@ export default function UserList(){
                         <Heading size="lg" fontWeight="normal">
                             Listagem de usuários
                         </Heading>
-                        <Link href="users/create" passHref>
+                        <Link href="/users/create" passHref>
                             <Button
                             as="a"
                             size="sm"
@@ -44,34 +53,73 @@ export default function UserList(){
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                                    <Checkbox colorScheme="pink"/>
-                                </Th>
-                                <Th>Usuários</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="6"></Th>
-                            </Tr>
-                        </Thead>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner/>
+                        </Flex>
+                    ) : error ?(
+                        <Flex justify="center" align="center">
+                            Falha ao obter dados dos usuários.
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                                            <Checkbox colorScheme="pink"/>
+                                        </Th>
+                                        <Th>Usuários</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width="6"></Th>
+                                    </Tr>
+                                </Thead>
 
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink"/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Warlike Richard</Text>
-                                        <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de abril, 2022
-                                </Td>}
-                                    {isWideVersion &&
-                                    <Td>
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink"/>
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Warlike Richard</Text>
+                                                <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>
+                                            04 de abril, 2022
+                                        </Td>}
+                                            {isWideVersion &&
+                                            <Td>
+                                                <Button
+                                                    as="a"
+                                                    size="sm"
+                                                    fontSize="sm"
+                                                    colorScheme="purple"
+                                                    leftIcon={<Icon
+                                                    as={RiPencilLine}
+                                                    fontSize="16"
+                                                    />}>
+                                                        Editar
+                                                </Button>
+                                            </Td>
+                                            }                              
+                                    </Tr>
+
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink"/>
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Warlike Richard</Text>
+                                                <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>
+                                            04 de abril, 2022
+                                        </Td>}
+                                        {isWideVersion && <Td>
                                         <Button
                                             as="a"
                                             size="sm"
@@ -83,70 +131,43 @@ export default function UserList(){
                                             />}>
                                                 Editar
                                         </Button>
-                                    </Td>
-                                    }                              
-                            </Tr>
+                                        </Td>}
+                                    </Tr>
 
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink"/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Warlike Richard</Text>
-                                        <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de abril, 2022
-                                </Td>}
-                                {isWideVersion && <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon
-                                    as={RiPencilLine}
-                                    fontSize="16"
-                                    />}>
-                                        Editar
-                                </Button>
-                                </Td>}
-                            </Tr>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink"/>
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Warlike Richard</Text>
+                                                <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>
+                                            04 de abril, 2022
+                                        </Td>}
+                                        {isWideVersion && <Td>
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="sm"
+                                            colorScheme="purple"
+                                            leftIcon={<Icon
+                                            as={RiPencilLine}
+                                            fontSize="16"
+                                            />}>
+                                                Editar
+                                        </Button>
+                                        </Td>}
+                                    </Tr>
+                                </Tbody>
 
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink"/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Warlike Richard</Text>
-                                        <Text fontSize="sm" color="gray.300">warlikerichard@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de abril, 2022
-                                </Td>}
-                                {isWideVersion && <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon
-                                    as={RiPencilLine}
-                                    fontSize="16"
-                                    />}>
-                                        Editar
-                                </Button>
-                                </Td>}
-                            </Tr>
-                        </Tbody>
+                            </Table>
 
-                    </Table>
-
-                    <Pagination />
+                            <Pagination />
+                        </>
+                    )}
             </Box>
             </Flex>
         </Box>
