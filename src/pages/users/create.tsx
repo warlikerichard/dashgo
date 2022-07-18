bea426c798b370d54bc2511012404ee5cc5a6803
Warlike Richard <warlikerichard@hotmail.com>
2022-07-17 23:54:00 -0300

    fixes

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,6 +6,10 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from "../../components/Form/Input";
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
+import { useMutation } from 'react-query'
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
     name: string;
@@ -25,6 +29,23 @@ const CreateUserFormSchema = yup.object().shape({
   })
 
 export default function CreateUser(){
+    const router = useRouter()
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user:{
+          ...user,
+          created_at: new Date(),
+        }
+      })
+
+      return response.data.user;
+    }, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users')
+      }
+    })
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(CreateUserFormSchema)
     });
@@ -32,8 +53,9 @@ export default function CreateUser(){
     const {errors} = formState;
 
     const handleCreateUser : SubmitHandler<CreateUserFormData> = async (data)=>{
-        await new Promise(resolve => setTimeout(resolve, 2*1000))
-        console.log(data)
+        await createUser.mutateAsync(data)
+
+        router.push('/users')
       }
     
 
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -9,6 +9,9 @@ type User = {
 
 export function makeServer(){
     const server = createServer({
+        serializers:{
+            application: ActiveModelSerializer
+        },
         models:{
             user: Model.extend<Partial<User>>({})
         },
@@ -44,7 +47,9 @@ export function makeServer(){
                 const pageEnd = Number(page) * Number(per_page)
 
                 const users = this.serialize(schema.all('user'))
-                .users.slice(pageStart, pageEnd)
+                .users
+                .sort((a, b) => a.createdAt - b.createdAt)
+                .slice(pageStart, pageEnd)
 
                 return new Response(
                     200,
